29e1eae272e5c9d02bb7831238196d5f62023ef9
Raluca-Cretu <35488769+Raluca-Cretu@users.noreply.github.com>
2018-03-16 21:17:00 +0100

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            /* Change these */
+            width: 500,
+            suffix: '_small',
+            quality: 30
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
